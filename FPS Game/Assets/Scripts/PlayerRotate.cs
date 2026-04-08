ac616690736a8e9faaba118f625edcfce4d72356
bocky1021 <138651588+bocky1021@users.noreply.github.com>
2026-04-08 11:19:22 +0900

    플레이어 회전

---
+++ FPS Game/Assets/Scripts/PlayerRotate.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public float rotSpeed = 5f;
+
+    float mx = 0;
+
+    private void Update()
+    {
+        float mouse_X = Input.GetAxis("Mouse X");
+
+        mx += mouse_X * rotSpeed;
+
+        transform.eulerAngles = new Vector3(0, mx, 0);
+    }
+}
